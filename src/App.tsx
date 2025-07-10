7357e05bbe92057774ee7df88e5a6431634172b6
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-07-10 14:10:32 +0100

    Fix issue not showing numbers when checked show numbees, change show numbers checkbox position

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import moment from "moment-timezone";
 import Clock from "./components/Clock/page";
+import ClockSm from "./components/Clock-sm/page";
+import Clockmd from "./components/Clock-md/page";
+import AnalogeClock from "./components/ClockAnalogue/page";
 
 let theme = {
   key: "light",
@@ -58,51 +61,54 @@ function App() {
 
   return (
     <div className="h-screen max-w-full">
-      <div className="inline-block relative w-64">
-        <label
-          htmlFor="small"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Select Time Zone
-        </label>
-        <select
-          id="small"
-          className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          value={selectedValue}
-          onChange={setCountry}
-        >
-          <option selected>Choose a country</option>
-          {timeZones.map((country) => (
-            <option value={country}>{country.match(/[^/]+$/)}</option>
-          ))}
-        </select>
+      <div>
+        <div className="inline-block relative w-64">
+          <label
+            htmlFor="small"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select Time Zone
+          </label>
+          <select
+            id="small"
+            className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={selectedValue}
+            onChange={setCountry}
+          >
+            <option selected>Choose a country</option>
+            {timeZones.map((country) => (
+              <option value={country}>{country.match(/[^/]+$/)}</option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <input
+            id="default-checkbox"
+            type="checkbox"
+            onChange={handleCheckboxChange}
+            checked={isChecked}
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="default-checkbox"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Show numbers
+          </label>
+        </div>
       </div>
-      <div className="flex items-center mb-4">
-        <input
-          id="default-checkbox"
-          type="checkbox"
-          onChange={handleCheckboxChange}
-          checked={isChecked}
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
-        <label
-          htmlFor="default-checkbox"
-          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          Show numbers
-        </label>
-      </div>
-      <div className="container m-auto grid grid-cols-3 gap-4 md:grid-cols-5 lg:grid-cols-4">
+
+      <div className="container m-auto grid grid-cols-3 gap-4  md:grid-cols-5 lg:grid-cols-4">
         {count.map((value) => (
           <div className="">
-            <Clock key={value} status={isChecked}></Clock>
+            <Clock key={value} isChecked={isChecked}></Clock>
           </div>
         ))}
         <div className="group relative flex cursor-pointer items-center justify-center text-sm">
           <button
             onClick={addClock}
             className="w-40 h-40  rounded-xl flex items-center justify-center text-3xl text-gray-600 bg-white/40 shadow hover:bg-gray-100"
-            aria-label="Add card"
+            aria-label="Add clock"
           >
             +
           </button>
