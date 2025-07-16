4b27c4314e8acff57105778bddb81cf34aee9804
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-07-16 13:03:41 +0100

    Add react select selector to search time-zone region, Replace add clock widget with the add button, Add scaling clock according the face

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-
 import "./App.css";
 import moment from "moment-timezone";
 import Clock from "./components/Clock/page";
+import Select from "react-select";
 
 let theme = {
   key: "light",
@@ -33,83 +33,101 @@ let size = {
   },
 };
 
-function App() {
-  const [selectedValue, setSelectedValue] = useState("");
-  const timeZones = moment.tz.names();
-  const [count, setCount] = useState<number[]>([1]);
+const customStyles = {
+  control: (provided: any) => ({
+    ...provided,
+    alignItems: "flex-start",
+    fontSize: "small",
+    color: "grey",
+    minHeight: "none",
+    padding: "none",
+    paddingTop: "0px",
+    paddingBottom: "0px",
+  }),
+  option: (provided: any) => ({
+    ...provided,
+    fontSize: "small",
+    color: "grey",
+  }),
+};
 
+function App() {
+  const [count, setCount] = useState<number[]>([1]);
   const [isChecked, setIsChecked] = useState(false);
+  const [timezone, setTimeZone] = useState<{
+    value: string;
+    label: string | undefined;
+  }>({ value: "", label: "" });
+  const [options, setOptions] = useState<
+    { value: string; label: string | undefined }[]
+  >([]);
 
   const handleCheckboxChange = (event: any) => {
     console.log("Check box checked");
     setIsChecked(event.target.checked);
   };
 
-  const setCountry = (event: any) => {
-    console.log("Country Value: ", event.target.value);
-    setSelectedValue(event.target.value);
-  };
-
   const addClock = () => {
     setCount([...count, count.length + 1]);
-    console.log("Clicked Add More");
-    console.log(count);
+    //setShow(true);
   };
+
+  useEffect(() => {
+    let mapOptions = moment.tz.names().map((country) => {
+      let a = country.match(/[^/]+$/) || [];
+      let val = { value: country, label: a[0] };
+      return val;
+    });
+
+    setOptions(mapOptions);
+  }, []);
 
   return (
     <div className="h-screen max-w-full">
       <div>
-        <div className="inline-block relative w-64">
-          <label
-            htmlFor="small"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Select Time Zone
-          </label>
-          <select
-            id="small"
-            className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={selectedValue}
-            onChange={setCountry}
-          >
-            <option selected>Choose a country</option>
-            {timeZones.map((country) => (
-              <option value={country}>{country.match(/[^/]+$/)}</option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-4">
-          <input
-            id="default-checkbox"
-            type="checkbox"
-            onChange={handleCheckboxChange}
-            checked={isChecked}
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            htmlFor="default-checkbox"
-            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Show numbers
-          </label>
+        <div className="flex  justify-center gap-2 mb-10">
+          <div className="w-50">
+            <Select
+              options={options}
+              styles={customStyles}
+              onChange={(option: any) => {
+                setTimeZone(option);
+              }}
+            />
+          </div>
+          <div className="content-center">
+            <button
+              className="bg-white hieght-3 hover:bg-gray-100 text-gray-500 text-xs  w-10 h-6 border border-gray-400 rounded shadow"
+              onClick={addClock}
+            >
+              Add
+            </button>
+          </div>
+
+          <div className="content-center" dir="ltr">
+            <input
+              id="default-checkbox"
+              type="checkbox"
+              onChange={handleCheckboxChange}
+              checked={isChecked}
+              className="text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="default-checkbox"
+              className="ms-2 text-xs font-medium text-gray-900 dark:text-gray-300"
+            >
+              Show numbers
+            </label>
+          </div>
         </div>
       </div>
 
-      <div className="container m-auto grid grid-cols-3 gap-4  md:grid-cols-5 lg:grid-cols-4">
+      <div className="container m-auto grid gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {count.map((value) => (
-          <div className="">
+          <div className="sm:scale-75 md:scale-75 lg:scale-75">
             <Clock key={value} isChecked={isChecked}></Clock>
           </div>
         ))}
-        <div className="group relative flex cursor-pointer items-center justify-center text-sm">
-          <button
-            onClick={addClock}
-            className="w-40 h-40  rounded-xl flex items-center justify-center text-3xl text-gray-600 bg-white/40 shadow hover:bg-gray-100"
-            aria-label="Add clock"
-          >
-            +
-          </button>
-        </div>
       </div>
     </div>
   );
