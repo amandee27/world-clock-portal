03bc898d21ce9b0998976ecaf473847d83231991
Amandee Ellawala <amandee.manushika.e@gmail.com>
2025-10-16 16:34:06 +0100

    Fix layout between the clock, delete button and the text

--- src/components/Clock/page.tsx
+++ src/components/Clock/page.tsx
@@ -2,7 +2,14 @@ import React, { useEffect, useState } from "react";
 import moment from "moment";
 const clockNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-function Clock({ timezone, isChecked, theme, currentDateTime }: any) {
+function Clock({
+  timezone,
+  isChecked,
+  theme,
+  currentDateTime,
+  isHovering,
+  deleteClock,
+}: any) {
   const [currentTime, setCurrentTime] = useState(currentDateTime);
   const [clockTheme, setClockTheme] = useState(theme);
   const [showNumbers, setShowNumbers] = useState(false);
@@ -67,56 +74,97 @@ function Clock({ timezone, isChecked, theme, currentDateTime }: any) {
   }, [theme]);
 
   return (
-    <div>
-      <div className="group relative flex cursor-pointer items-center justify-center text-sm">
-        <div
-          className={`${clockTheme.value.clockFace} w-56 h-56 shrink-0 grow-0 relative flex items-center justify-center rounded-full`}
-        >
-          <section className="box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25); absolute z-50 flex h-4 w-4 justify-center">
-            {clockNumbers.map((num) => (
-              <span key={num} className="inline-block">
-                <section
-                  key={num}
-                  className={`${clockTheme.value.marks} h-27 w-1/10   absolute bottom-1.5 z-30 origin-bottom`}
-                  style={{ transform: `rotate(calc(${num}*6*5deg))` }}
+    <div className="rounded-xl  hover:shadow-lg hover:bg-gray-500/20 transition-all ">
+      <div className="relative p-4 flex flex-col items-center justify-between text-sm">
+        <div className="absolute top-2 right-2 text-white hover:text-red-400">
+          {isHovering && timezone.value !== "" && (
+            <button
+              className="text-white	"
+              onClick={() => timezone.label && deleteClock(timezone.label)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="20"
+                height="20"
+                viewBox="0,0,256,256"
+                style={{ fill: "#FFFFFF" }}
+              >
+                <g
+                  fill="#ffffff"
+                  fillRule="nonzero"
+                  stroke="none"
+                  strokeWidth="1"
+                  strokeLinecap="butt"
+                  strokeLinejoin="miter"
+                  strokeMiterlimit="10"
+                  strokeDasharray=""
+                  strokeDashoffset="0"
+                  fontFamily="none"
+                  fontWeight="none"
+                  fontSize="none"
+                  textAnchor="none"
+                  style={{ mixBlendMode: "normal" }}
                 >
-                  {showNumbers && (
-                    <span
-                      className="inline-block absolute justify-left"
-                      style={{
-                        transform: `translate(-50%, 0%) rotate(calc(${num}*(-6*5deg)))`,
-                      }}
-                    >
-                      {num}
-                    </span>
-                  )}
-                </section>
-              </span>
-            ))}
+                  <g transform="scale(8.53333,8.53333)">
+                    <path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.627 5.373,12 12,12c6.627,0 12,-5.373 12,-12c0,-6.627 -5.373,-12 -12,-12zM16.414,15c0,0 3.139,3.139 3.293,3.293c0.391,0.391 0.391,1.024 0,1.414c-0.391,0.391 -1.024,0.391 -1.414,0c-0.154,-0.153 -3.293,-3.293 -3.293,-3.293c0,0 -3.139,3.139 -3.293,3.293c-0.391,0.391 -1.024,0.391 -1.414,0c-0.391,-0.391 -0.391,-1.024 0,-1.414c0.153,-0.154 3.293,-3.293 3.293,-3.293c0,0 -3.139,-3.139 -3.293,-3.293c-0.391,-0.391 -0.391,-1.024 0,-1.414c0.391,-0.391 1.024,-0.391 1.414,0c0.154,0.153 3.293,3.293 3.293,3.293c0,0 3.139,-3.139 3.293,-3.293c0.391,-0.391 1.024,-0.391 1.414,0c0.391,0.391 0.391,1.024 0,1.414c-0.153,0.154 -3.293,3.293 -3.293,3.293z"></path>
+                  </g>
+                </g>
+              </svg>
+            </button>
+          )}
+        </div>
+        <div className="flex-1 flex items-center justify-center">
+          <div
+            className={`${clockTheme.value.clockFace} w-56 h-56 shrink-0 grow-0 relative flex items-center justify-center rounded-full`}
+          >
+            <section className="box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25); absolute z-50 flex h-4 w-4 justify-center">
+              {clockNumbers.map((num) => (
+                <span key={num} className="inline-block">
+                  <section
+                    key={num}
+                    className={`${clockTheme.value.marks} h-27 w-1/10   absolute bottom-1.5 z-30 origin-bottom`}
+                    style={{ transform: `rotate(calc(${num}*6*5deg))` }}
+                  >
+                    {showNumbers && (
+                      <span
+                        className="inline-block absolute justify-left"
+                        style={{
+                          transform: `translate(-50%, 0%) rotate(calc(${num}*(-6*5deg)))`,
+                        }}
+                      >
+                        {num}
+                      </span>
+                    )}
+                  </section>
+                </span>
+              ))}
 
-            {/* Clock center */}
-            <span
-              className={`${clockTheme.value.hand.center} h-2.5 w-2.5 bottom-[1px] absolute z-50 flex rounded-full`}
-            ></span>
-            {/* Second hand */}
-            <span
-              className={`${clockTheme.value.hand.second} h-23 w-1/10 absolute bottom-1.5 z-30 w-1 origin-bottom rounded-md`}
-              style={timing.updateSeconds}
-            ></span>
-            {/* Minute hand */}
-            <span
-              className={`${clockTheme.value.hand.minute} h-22 w-1/6 absolute bottom-1.5 z-20 origin-bottom rounded-md`}
-              style={timing.updateMinutes}
-            ></span>
-            {/* Hour hand */}
-            <span
-              className={`${clockTheme.value.hand.hour} h-15 w-1/5 absolute bottom-1.5 z-10 origin-bottom divide-zinc-100 rounded-md`}
-              style={timing.updateHours}
-            ></span>
-          </section>
+              {/* Clock center */}
+              <span
+                className={`${clockTheme.value.hand.center} h-2.5 w-2.5 bottom-[1px] absolute z-50 flex rounded-full`}
+              ></span>
+              {/* Second hand */}
+              <span
+                className={`${clockTheme.value.hand.second} h-23 w-1/10 absolute bottom-1.5 z-30 w-1 origin-bottom rounded-md`}
+                style={timing.updateSeconds}
+              ></span>
+              {/* Minute hand */}
+              <span
+                className={`${clockTheme.value.hand.minute} h-22 w-1/6 absolute bottom-1.5 z-20 origin-bottom rounded-md`}
+                style={timing.updateMinutes}
+              ></span>
+              {/* Hour hand */}
+              <span
+                className={`${clockTheme.value.hand.hour} h-15 w-1/5 absolute bottom-1.5 z-10 origin-bottom divide-zinc-100 rounded-md`}
+                style={timing.updateHours}
+              ></span>
+            </section>
+          </div>
         </div>
       </div>
-      <div>
+      <div className="text-center">
         <h1 className="text-l font-semibold text-stone-50 mt-4">
           {timezone.value ? timezone.label : "Local"}
         </h1>
